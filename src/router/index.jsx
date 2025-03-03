7d207f43359147b532f31816ff8964c7f6596989
shinsouhitoshi1203 <24521353@gm.uit.edu.vn>
2025-03-03 12:41:47 +0700

    removed test page

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home";
-import TestPage from "../pages/TestPage";
 import Error404 from "../pages/Error/404";
 import HelpPage from "../pages/Help";
 
@@ -12,7 +11,6 @@ const router = createBrowserRouter([
 		children: [
 			{ path: "", element: <Home /> },
 			{ path: "help", element: <HelpPage /> },
-			// { path: "test", element: <TestPage /> },
 			{ path: "*", element: <Error404 /> }
 		]
 	}
